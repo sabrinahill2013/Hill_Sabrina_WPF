001584b00670b49904c2f014d7d685c0c3e32ef8
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-21 23:57:36 -0400

    Changes for Conditional Logic - Relational Expressions

--- Conditionals_Screencast/Conditionals_Screencast/js/script.js
+++ Conditionals_Screencast/Conditionals_Screencast/js/script.js
@@ -1,11 +1,13 @@
-//Conditional Logic
+//Conditional Logic - Relational Expressions
 
-var oldEnough = false;
+var kidHeight = 40;
+var minHeight = 48
 
-//if the child is old enough, print to the console "you can ride"
-if(oldEnough){
+//if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+if(kidHeight > 48){
    //code performed if the condition is true
     console.log("You can ride the coaster!");
 }
-console.log("what comes after")
+
 
