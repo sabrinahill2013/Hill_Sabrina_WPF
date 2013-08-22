eacb116439687573cb812ef811a843de65561aa2
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-21 23:52:23 -0400

    Screencast week 3 conditionals example

---
+++ Conditionals_Screencast/Conditionals_Screencast/js/script.js
@@ -0,0 +1,11 @@
+//Conditional Logic
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride"
+if(oldEnough){
+   //code performed if the condition is true
+    console.log("You can ride the coaster!");
+}
+console.log("what comes after")
+
