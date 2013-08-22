fee29d72fe9dde1ebaba57def05b05ce80f47513
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-22 00:06:11 -0400

    Changes from Conditional Logic - With an Expression-If and Else

--- Conditionals_Screencast/Conditionals_Screencast/js/script.js
+++ Conditionals_Screencast/Conditionals_Screencast/js/script.js
@@ -1,14 +1,16 @@
-//Conditional Logic - With an Expression
+//Conditional Logic - With an Expression-If and Else
 
-var kidHeight = 47;
+var kidHeight = 52;
 var minHeight = 48;
-var sneakerLift = 2
+
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerLift > minHeight){
+if(kidHeight> minHeight){
    //code performed if the condition is true
     console.log("You can ride the coaster!");
+}else{
+    //code performed if the condition is false
+    console.log("Sorry kid, you've got some growing to do first!")
 }
-
 
