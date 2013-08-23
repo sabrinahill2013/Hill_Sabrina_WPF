3239c8622dce9142c048604f09a8640122d26506
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-23 00:11:08 -0400

    Changes to code that show an ifElse statment using a relational operator Hill_Sabrina_Conditionals_Personal

--- Conditionals_Personal/Conditionals_Personal/js/script.js
+++ Conditionals_Personal/Conditionals_Personal/js/script.js
@@ -9,6 +9,16 @@
 
 var monthlySavings = 250; //After taking the users input we found that they will save 250 for the month.
 
-(monthlySavings > 200) ? console.log("You can take a trip") : console.log("You have to stay in town") //This is a ternary example of the scenario
+//(monthlySavings > 200) ? console.log("You can take a trip") : console.log("You have to stay in town") //This is a ternary example of the scenario
 
+
+//The following is an example of if and else also including a relational operator
+
+var monthlySavings = 250; //After taking the users input we found that they will save 250 for the month.
+var tripCost = 350
+
+if(monthlySavings > tripCost){
+    //code performed if condition is true
+    console.log("You can take a trip"); //this will print to the console if my trip cost is less than what I saved for the month
+}else{console.log("You need to save more money before you can take a trip")}//condition performed if the trip cost is more than what I save this month
 
