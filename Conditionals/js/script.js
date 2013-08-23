8a5814fbc5cf75d1200931e3564797fab9179e66
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-22 20:55:39 -0400

    Week 3 Conditionals_Personal

---
+++ Conditionals/js/script.js
@@ -0,0 +1,11 @@
+//Sabrina Hill Conditionals Assignment
+
+var weeklySavings = prompt("How much money do you save a week?"); //The user will input how much they save a week
+var numberOfWeeksInMonth = prompt("How many weeks are in this month?");//The user will input how many weeks are in the current month
+var monthlySavings = weeklySavings * numberOfWeeksInMonth; //calculates how much I will save depending on how many weeks are in the month.
+console.log(monthlySavings); //Prints to the console
+
+alert("You will save " + monthlySavings); //This will calculate and tell me how much I will save for the month.
+
+
+
