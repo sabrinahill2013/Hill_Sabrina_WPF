dfeb7711002aa5dd7b8f2adc0102889f25766d38
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-22 21:05:50 -0400

    Week 3 Conditionals_Personal- Ternary Example

--- Conditionals_Personal/Conditionals_Personal/js/script.js
+++ Conditionals_Personal/Conditionals_Personal/js/script.js
@@ -1,8 +1,14 @@
 //Sabrina Hill Conditionals Assignment
 
-var weeklySavings = prompt("How much money do you save a week?"); //The user will input how much they save a week
-var numberOfWeeksInMonth = prompt("How many weeks are in this month?");//The user will input how many weeks are in the current month
-var monthlySavings = weeklySavings * numberOfWeeksInMonth; //calculates how much I will save depending on how many weeks are in the month.
-console.log(monthlySavings); //Prints to the console
+//var weeklySavings = prompt("How much money do you save a week?"); //The user will input how much they save a week
+//var numberOfWeeksInMonth = prompt("How many weeks are in this month?");//The user will input how many weeks are in the current month
+//var monthlySavings = weeklySavings * numberOfWeeksInMonth; //calculates how much I will save depending on how many weeks are in the month.
+//console.log(monthlySavings); //Prints to the console
 
-alert("You will save " + monthlySavings); //This will calculate and tell me how much I will save for the month.
+//alert("You will save " + monthlySavings); //This will calculate and tell me how much I will save for the month.
+
+var monthlySavings = 250; //After taking the users input we found that they will save 250 for the month.
+
+(monthlySavings > 200) ? console.log("You can take a trip") : console.log("You have to stay in town") //This is a ternary example of the scenario
+
+
