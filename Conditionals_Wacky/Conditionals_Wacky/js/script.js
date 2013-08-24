d91376faa7037b7dd8d4cc26386a7dce4784a39c
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-23 23:41:54 -0400

    This is the first commit for the Conditionals_Wacky subproject. I made a mistake and commited the wrong subproject in my last commit.

---
+++ Conditionals_Wacky/Conditionals_Wacky/js/script.js
@@ -0,0 +1,35 @@
+//Sabrina Hill Conditionals_Wacky August 22nd, 2013
+
+
+
+//The following code will be calculating how many slices of pizza each person at a dinner party can have based on ordering 8
+
+//var numberOfAttendees = prompt("How many people are coming to the pizza party?"); //The user will input how many people are coming to the party
+//var numberOfPizzasBeingOrdered = prompt("How many pizzas are you ordering?");
+//var numberOfSlicesInOnePie = prompt("How many slices come in a whole pizza");//The user will input how many slices are in one whole pizza
+//var howMuchEachPersonCanHave = numberOfAttendees * numberOfSlicesInOnePie /5; //calculates how many slices each person at the party can have
+
+
+//console.log(howMuchEachPersonCanHave); //Prints to the console how many slices each person can have
+
+//alert("Each person can have this many slices: " +  howMuchEachPersonCanHave); //This will calculate how many slices each person at the pizza party can have
+
+//var howMuchEachPersonCanHave = 8; //After taking the users input we found that each person at the pizza party can have 8 slices of pizza
+
+//The following is a ternary example of the scenario including a relational operator
+
+//(howMuchEachPersonCanHave > 5) ? console.log("This will be enough to fill each person up") : console.log("You may need to order some more pizzas")
+
+//The following is an example of if and else also including a relational operator
+
+var numberOfSlicesInONePie = 8; //These number are representing dollars per hour
+var howMuchEachPersonCanHave = 6; //These numbers are representing dollars per hour
+
+
+if(howMuchEachPersonCanHave < numberOfSlicesInONePie){//code performed if condition is true
+console.log("You will have enough to feed everyone"); //this will print to the console if the person will be getting what they want to get an hour
+}else{console.log("You may need to order more pizza")}//condition performed if the desired hourly increase is less than what the person wants
+
+
+
+
