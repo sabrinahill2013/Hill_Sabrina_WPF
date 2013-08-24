8a0ca7a5f3b15a56857325610cff46744d7a66fc
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-23 22:42:03 -0400

    Hill_Sabrina Conditionals_Industry Related

---
+++ Conditionals_IndustryRelated/Conditionals_IndustryRelated/js/script.js
@@ -0,0 +1,13 @@
+//Sabrina Hill Conditionals_Industry Related  August 22nd, 2013
+
+
+//The following code will be calculating how much more an hour a person will make if they get a raise every three months for two years.
+
+var raiseAmount = prompt("How much are your raises?"); //The user will input how much of a raise they will get
+var numberOfRaises = prompt("How many raises will you get in two years");//The user will input how many raises they will get over two years
+var hourlyIncrease = raiseAmount * numberOfRaises; //calculates how much more an hour the person will make after two years of raises every three months
+
+console.log(hourlyIncrease); //Prints to the console how much more an hour they will be making
+
+alert("You will make this much more an hour if you get a raise every three months for two years: " +  hourlyIncrease); //This will calculate how much more the person will be making an hour based on the number of raises they get
+
