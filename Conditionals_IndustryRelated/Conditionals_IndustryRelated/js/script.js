8e8b10ea541101dfe77be39692baba40e429cea0
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-23 22:58:15 -0400

    Hill_Sabrina Conditionals_Industry Related

--- Conditionals_IndustryRelated/Conditionals_IndustryRelated/js/script.js
+++ Conditionals_IndustryRelated/Conditionals_IndustryRelated/js/script.js
@@ -11,10 +11,19 @@
 
 //alert("You will make this much more an hour if you get a raise every three months for two years: " +  hourlyIncrease); //This will calculate how much more the person will be making an hour based on the number of raises they get
 
-var hourlyIncrease = 4; //After taking the users input we found that they will make an extra four dollars an hour if their raises are 50 cent
+//var hourlyIncrease = 4; //After taking the users input we found that they will make an extra four dollars an hour if their raises are 50 cent
 
 
 //The following is a ternary example of the scenario including a relational operator
 
-(hourlyIncrease >= 4) ? console.log("You are considered pay grade four") : console.log("You are still pay grade three")
+//(hourlyIncrease >= 4) ? console.log("You are considered pay grade four") : console.log("You are still pay grade three")
 
+//The following is an example of if and else also including a relational operator
+
+var hourlyIncrease = 4; //These number are representing dollars per hour
+var desiredIncrease = 6; //These numbers are representing dollars per hour
+
+
+if(hourlyIncrease > desiredIncrease){//code performed if condition is true
+console.log("You have received your desired hourly pay"); //this will print to the console if the person will be getting what they want to get an hour
+}else{console.log("You have to get a few more raises before you will have the desired hourly pay")}//condition performed if the desired hourly increase is less than what the person wants
