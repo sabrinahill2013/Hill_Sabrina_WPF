27cf5c2781db9dc1c8273fff9741f289e4586ac4
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-29 23:01:39 -0400

    Functions_Personal Setting up the promts

---
+++ Functions_Personal/Functions_Personals/js/script.js
@@ -0,0 +1,15 @@
+//Hill_Sabrina Functions_Personal August 29, 2013
+
+
+//The following code will be calculating how many showers is take in a month
+
+var numberOfShowersDaily = prompt("How many showers do you take in one day?"); //I will input how many showers I take a day
+var numberOfDaysInMonth = prompt("How many days are in the current month?");
+var numberOfShowersPerMonth = numberOfShowersDaily * numberOfDaysInMonth ; //calculates how many showers I will take this month
+
+
+console.log(numberOfShowersPerMonth); //Prints to the console how many showers I will take in this current month
+
+alert("You will have taken this many showers at the end of the month: " + numberOfShowersPerMonth); //This will calculate how many showers I will take in the current month
+
+
