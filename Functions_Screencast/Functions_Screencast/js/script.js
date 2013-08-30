93a96036611d57c31ba4bed2f47f7f3a78ca2b07
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-29 20:29:10 -0400

    Function Screencast Follow Along

---
+++ Functions_Screencast/Functions_Screencast/js/script.js
@@ -0,0 +1,15 @@
+//Sabrina Hill Functions Executions ScreenCast
+
+function outptMsg(){
+    console.log("Hello World")
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+
+}
+
+calcArea();
