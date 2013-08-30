edd3becbfe581c6ee37ab3cdb4c9b8aad361fc6c
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-29 23:08:52 -0400

    Functions_Personal changes: Regular/Named Function added to code

--- Functions_Personal/Functions_Personals/js/script.js
+++ Functions_Personal/Functions_Personals/js/script.js
@@ -3,13 +3,24 @@
 
 //The following code will be calculating how many showers is take in a month
 
-var numberOfShowersDaily = prompt("How many showers do you take in one day?"); //I will input how many showers I take a day
-var numberOfDaysInMonth = prompt("How many days are in the current month?");
-var numberOfShowersPerMonth = numberOfShowersDaily * numberOfDaysInMonth ; //calculates how many showers I will take this month
+//var numberOfShowersDaily = prompt("How many showers do you take in one day?"); //I will input how many showers I take a day
+//var numberOfDaysInMonth = prompt("How many days are in the current month?");
+//var numberOfShowersPerMonth = numberOfShowersDaily * numberOfDaysInMonth ; //calculates how many showers I will take this month
 
 
-console.log(numberOfShowersPerMonth); //Prints to the console how many showers I will take in this current month
+//console.log(numberOfShowersPerMonth); //Prints to the console how many showers I will take in this current month
 
-alert("You will have taken this many showers at the end of the month: " + numberOfShowersPerMonth); //This will calculate how many showers I will take in the current month
+//alert("You will have taken this many showers at the end of the month: " + numberOfShowersPerMonth); //This will calculate how many showers I will take in the current month
+
+//The following is a regular "named" function example of the above scenario
+
+function numberOfShowersPerMonth(){
+    var numberOfShowersDaily = 2;
+    var numberOfDaysInMonth = 31;
+    var numberOfShowersPerMonth = numberOfShowersDaily * numberOfDaysInMonth;
+    console.log(numberOfShowersPerMonth)
+}
+numberOfShowersPerMonth();
+
 
 
