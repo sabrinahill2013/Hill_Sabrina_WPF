658d0028567f73c315a1569b13641c43fc24c2ba
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-31 18:29:24 -0400

    Functions_Industry Changes Made If Else using a logical operator

--- Functions_Industry/Functions_Industry/js/script.js
+++ Functions_Industry/Functions_Industry/js/script.js
@@ -24,12 +24,24 @@
 
 //The following is an Anonymous Function
 
-var totalNumberOfFliersToMake = function(numberOfClients, numberOfRequestedFliers){ //defining the function
-var totalNumberOfFliersToMake = numberOfClients * numberOfRequestedFliers;
-return totalNumberOfFliersToMake;
-}
+//var totalNumberOfFliersToMake = function(numberOfClients, numberOfRequestedFliers){ //defining the function
+//var totalNumberOfFliersToMake = numberOfClients * numberOfRequestedFliers;
+//return totalNumberOfFliersToMake;
+//}
 
-var a = totalNumberOfFliersToMake(5, 100); //invoking or "calling out" the function
+//var a = totalNumberOfFliersToMake(5, 100); //invoking or "calling out" the function
 
-console.log(a);
+//console.log(a);
+
+//The following is an ifElse including a logical operator to see if I have enough days to make all the fliers
+
+var estimatedCompletionRate = 125; //number is representing days, so an estimated number of 125 completed fliers per day
+var numberCompletedAlready = 100;
+var averageCompletionRate = 100; //100 fliers per day
+var deadline = 5 //number representing days, so I have 5 days to complete
+
+if(averageCompletionRate < estimatedCompletionRate || numberCompletedAlready){//code performed if condition is true
+console.log("You need to work a little faster!"); //this will print to the console if I need to speed up my work
+}else{console.log("You will complete the fliers by the deadline!")}//condition performed if I am completing enough fliers a day and will meet the deadline
+
 
