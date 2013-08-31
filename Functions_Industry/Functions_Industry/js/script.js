9d1355526c06973b19a3ee001097184e2056865f
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-08-31 18:12:00 -0400

    Functions_Industry Changes Made: Named Function

--- Functions_Industry/Functions_Industry/js/script.js
+++ Functions_Industry/Functions_Industry/js/script.js
@@ -3,12 +3,22 @@
 
 //The following code will be calculating the total amount of  fliers I will have to make for my clients assuming each client needs the same amount of fliers
 
-var numberOfClients = prompt("How many clients do you have that need fliers?"); //I will input how many showers I take a day
-var numberOfFliersRequested = prompt("How many fliers do each client need?");
-var totalNumberOfFliersToMake = numberOfClients * numberOfFliersRequested ; //calculates how many showers I will take this month
+//var numberOfClients = prompt("How many clients do you have that need fliers?"); //I will input how many showers I take a day
+//var numberOfFliersRequested = prompt("How many fliers do each client need?");
+//var totalNumberOfFliersToMake = numberOfClients * numberOfFliersRequested ; //calculates how many showers I will take this month
 
 
-console.log(totalNumberOfFliersToMake); //Prints to the console how many fliers I need to make
+//console.log(totalNumberOfFliersToMake); //Prints to the console how many fliers I need to make
 
-alert("You will need to make this many fliers: " + totalNumberOfFliersToMake); //This will calculate how many fliers I will need to make
+//alert("You will need to make this many fliers: " + totalNumberOfFliersToMake); //This will calculate how many fliers I will need to make
+
+//The following is a regular "named" function example of the above scenario
+
+function totalNumberOfFliersToMake(){
+var numberOfClients = 5;
+var numberOfRequestedFliers = 100;
+var totalNumberOfFliersToMake = numberOfClients * numberOfRequestedFliers;
+console.log(totalNumberOfFliersToMake);
+}
+totalNumberOfFliersToMake();
 
