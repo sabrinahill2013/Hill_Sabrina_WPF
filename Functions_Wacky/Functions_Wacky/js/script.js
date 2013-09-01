1dc12e5a37de6851692424829539216205d9abed
Sabrina Hill <sabrinahill2011@yahoo.com>
2013-09-01 16:52:34 -0400

    Functions_Wacky Changes Made: Named Function

--- Functions_Wacky/Functions_Wacky/js/script.js
+++ Functions_Wacky/Functions_Wacky/js/script.js
@@ -3,12 +3,24 @@
 
 //The following code will be calculating how many beers will be at the party if each person brings a case of 24 beers.
 
-var numberOfPeople = prompt("How many people are coming to the party?"); // Will prompt the person to input how many people will be coming to the party
-var numberOfCasesPerPerson = prompt("How many cases will each person bring?");
-var numberOfBeersPerCase = prompt("How many beers are in each case?");
-var totalNumberOfBeersAtParty = numberOfPeople * numberOfCasesPerPerson * numberOfBeersPerCase ; //calculates how many beers will be at the party
+//var numberOfPeople = prompt("How many people are coming to the party?"); // Will prompt the person to input how many people will be coming to the party
+//var numberOfCasesPerPerson = prompt("How many cases will each person bring?");
+//var numberOfBeersPerCase = prompt("How many beers are in each case?");
+//var totalNumberOfBeersAtParty = numberOfPeople * numberOfCasesPerPerson * numberOfBeersPerCase ; //calculates how many beers will be at the party
 
 
-console.log(totalNumberOfBeersAtParty); //Prints to the console how many beers in total will be at the party
+//console.log(totalNumberOfBeersAtParty); //Prints to the console how many beers in total will be at the party
 
-alert("There will be this many beers at the party: " + totalNumberOfBeersAtParty); //This will calculate how many total beers will be at the party
+//alert("There will be this many beers at the party: " + totalNumberOfBeersAtParty); //This will calculate how many total beers will be at the party
+
+//The following is a regular "named" function example of the above scenario
+
+function totalNumberOfBeersAtParty(){
+var numberOfPeople = 10;
+var numberOfCasesPerPerson = 1;
+var numberOfBeersPerCase = 24;
+var totalNumberOfBeersAtParty = numberOfPeople * numberOfCasesPerPerson * numberOfBeersPerCase;
+console.log(totalNumberOfBeersAtParty);
+}
+totalNumberOfBeersAtParty();
+
